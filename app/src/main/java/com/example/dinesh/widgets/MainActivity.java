f8cefd1e9f5c31a9ab800766e70d37f122c8a3fb
Dinesh Chabra <dinesh_chabra@yahoo.com>
2017-03-23 17:38:04 +0530

    Different button imgaes at different instances

--- app/src/main/java/com/example/dinesh/widgets/MainActivity.java
+++ app/src/main/java/com/example/dinesh/widgets/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.dinesh.widgets;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+
     }
+
+
+
+    public void addListenerOnButton() {
+
+        Button button1 = (Button)findViewById(R.id.butt1);
+
+        button1.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Toast.makeText(MainActivity.this,"Button Pressed",Toast.LENGTH_SHORT).show();
+
+
+            }
+
+        });
+
+    }
+
+
 }
