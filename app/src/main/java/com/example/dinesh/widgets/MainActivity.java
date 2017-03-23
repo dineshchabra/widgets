a73644480d3b0f075224147735fb30a24c98391e
Dinesh Chabra <dinesh_chabra@yahoo.com>
2017-03-23 20:00:44 +0530

    Edittext-Password with Multiple Listener

--- app/src/main/java/com/example/dinesh/widgets/MainActivity.java
+++ app/src/main/java/com/example/dinesh/widgets/MainActivity.java
@@ -2,12 +2,11 @@ package com.example.dinesh.widgets;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
+import android.view.KeyEvent;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
@@ -15,27 +14,53 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+    private EditText password;
+    private Button btnSubmit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addListenerOnButton();
+        addPassListener();
 
     }
 
+    public void addPassListener() {
 
+        password = (EditText) findViewById(R.id.txtPassword);
+        password.setOnKeyListener(new View.OnKeyListener() {
+
+                public boolean onKey(View v, int keyCode, KeyEvent event){
+
+                    if (keyCode == KeyEvent.KEYCODE_ENTER) {
+
+                        // display a floating message
+                        Toast.makeText(MainActivity.this,
+                                password.getText(), Toast.LENGTH_SHORT).show();
+                        return true; }
+
+
+
+                    return false;
+                }
+
+        });
+
+    }
 
     public void addListenerOnButton() {
 
-        Button button1 = (Button)findViewById(R.id.butt1);
+        password = (EditText) findViewById(R.id.txtPassword);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
-        button1.setOnClickListener(new OnClickListener() {
+        btnSubmit.setOnClickListener(new OnClickListener() {
 
             @Override
-            public void onClick(View arg0) {
+            public void onClick(View v) {
 
-                Toast.makeText(MainActivity.this,"Button Pressed",Toast.LENGTH_SHORT).show();
-
+                Toast.makeText(MainActivity.this, password.getText(),
+                        Toast.LENGTH_SHORT).show();
 
             }
 
